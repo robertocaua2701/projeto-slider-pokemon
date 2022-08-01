bd7e252f435645bb299473189441c112505d4e79
roberto cauã <106180519+robertocaua2701@users.noreply.github.com>
2022-08-01 15:39:43 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,38 @@
+
+const btnAvancar = document.getElementById("btn-avancar");
+const btnVoltar = document.getElementById("btn-voltar");
+const cartoes = document.querySelectorAll(".cartao"); 
+let cartaoAtual = 0;
+
+function EsconderCartaoSelecionado(){
+    const cartaoSelecionado = document.querySelector(".selecionado");
+    cartaoSelecionado.classList.remove("selecionado");
+} 
+
+function mostrarCartao(indiceCartao){
+    cartoes[indiceCartao].classList.add("selecionado"); 
+}
+
+btnAvancar.addEventListener("click", function () {
+    if(cartaoAtual=== cartoes.length - 1 ) return;
+
+    EsconderCartaoSelecionado();
+
+    cartaoAtual++;
+    mostrarCartao(cartaoAtual);   
+
+
+});
+
+btnVoltar.addEventListener("click", function () {
+
+    if(cartaoAtual === 0) return; 
+    
+    EsconderCartaoSelecionado();
+    
+    cartaoAtual--;
+    mostrarCartao(cartaoAtual); 
+
+});
+
+
